6234c1caf95e7a72a4a12a3fc841d6f6bc970002
Gianni Vallerini <gianni.vallerini@hotmail.com>
2025-08-25 11:11:47 +0200

    chore: add audio view

--- OperaVR_Unity/Assets/_OperaVR/Scripts/Popups/Quiz/QuizContentView.cs
+++ OperaVR_Unity/Assets/_OperaVR/Scripts/Popups/Quiz/QuizContentView.cs
@@ -10,17 +10,21 @@ namespace OperaVR
         [SerializeField]
         private ImageView _imageView;
 
-        //TODO AudioView
+        [SerializeField]
+        private TrackPlayer _trackPlayer;
 
         public void LoadContent(QuizContent content)
         {
+            _textView.gameObject.SetActive(content.Type == QuizContent.QuizContentType.Text);
+            _imageView.gameObject.SetActive(content.Type == QuizContent.QuizContentType.Image);
+            _trackPlayer.gameObject.SetActive(content.Type == QuizContent.QuizContentType.Audio);
             switch (content.Type)
             {
                 case QuizContent.QuizContentType.Text:
                     _textView.TextDisplayer.text = content.Text;
                     break;
                 case QuizContent.QuizContentType.Audio:
-                    //TODO
+                    _trackPlayer.LoadClip(content.AudioClip);
                     break;
                 case QuizContent.QuizContentType.Image:
                     _imageView.SetImageSprite(content.Image);
@@ -32,21 +36,21 @@ namespace OperaVR
         {
             _textView.SetSelected(isSelected);
             _imageView.SetSelected(isSelected);
-            //TODO AudioView
+            //TODO TrackPlayer
         }
 
         public void SetInteractable(bool isInteractable, bool isSelected)
         {
             _textView.SetInteractable(isInteractable);
             _imageView.SetInteractable(isInteractable, isSelected);
-            //TODO AudioView
+            //TODO TrackPlayer
         }
 
         public void SetHidden(bool isHidden, bool isSelected)
         {
             _textView.SetHidden(isHidden);
             _imageView.SetHidden(isHidden, isSelected);
-            //TODO AudioView
+            //TODO TrackPlayer
         }
     }
 }
