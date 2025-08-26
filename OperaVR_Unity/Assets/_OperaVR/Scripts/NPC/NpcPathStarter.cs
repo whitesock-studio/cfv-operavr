91232298b4a8442c68b7c4aac6ea4c264a492810
Gianni Vallerini <gianni.vallerini@hotmail.com>
2025-08-26 10:26:34 +0200

    chore: add features to npc

--- OperaVR_Unity/Assets/_OperaVR/Scripts/NPC/NpcPathStarter.cs
+++ OperaVR_Unity/Assets/_OperaVR/Scripts/NPC/NpcPathStarter.cs
@@ -1,14 +1,31 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace OperaVR
 {
     public class NpcPathStarter : MonoBehaviour
     {
+        [Header("Default run speed is 5.13")]
+        public float RunningSpeed = 5.13f;
+
+        [Header("Default walk speed is 3.20")]
+        public float WalkingSpeed = 3.20f;
+
         [SerializeField]
         private NPC _npc;
 
         [SerializeField]
         private Path _path;
+
+        [SerializeField]
+        private bool _activateOnStart;
+
+        [Header("Triggered when the path is complete\n(only for loop == false paths)")]
+        public UnityEvent OnComplete;
+
+        [Header("Triggered every time the path loops\n(only for loop == true paths)")]
+        public UnityEvent OnLoop;
 
         private bool _isOn;
         private int _currentIndex = 0;
@@ -20,6 +37,19 @@ namespace OperaVR
                 return;
             }
             StartPath();
+        }
+
+        private IEnumerator Start()
+        {
+            if (!_activateOnStart)
+            {
+                yield break;
+            }
+            while (!_npc.HasAvatar)
+            {
+                yield return null;
+            }
+            Activate();
         }
 
         private void Update()
@@ -36,6 +66,7 @@ namespace OperaVR
             _isOn = true;
             _currentIndex = 0;
             _npc.SetDestination(_path.Points[_currentIndex].position);
+            _npc.SetSpeeds(RunningSpeed, WalkingSpeed);
         }
 
         private void FollowPath()
@@ -56,8 +87,10 @@ namespace OperaVR
             {
                 _currentIndex %= _path.Points.Length;
                 _npc.SetDestination(_path.Points[_currentIndex].position);
+                OnLoop?.Invoke();
                 return;
             }
+            OnComplete?.Invoke();
             _isOn = false;
         }
     }
