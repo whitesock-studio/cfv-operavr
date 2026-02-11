ff0eaa88379ac48a9c2f741300d6b58293bc1e78
Gianni <>
2026-02-11 13:04:16 +0100

    chore: update sdk, add animator triggerer

---
+++ OperaVR_Unity/Assets/_OperaVR/Scripts/AnimatorTriggerer.cs
@@ -0,0 +1,61 @@
+using System;
+using UnityEngine;
+
+namespace OperaVR
+{
+    public class AnimatorTriggerer : MonoBehaviour
+    {
+        private Transform[] _children;
+        
+        private void Awake()
+        {
+            _children = GetComponentsInChildren<Transform>(true);
+        }
+
+        public void SetActive(string nameAndState)
+        {
+           var parts = nameAndState.Split(':');
+
+           foreach (var child in _children)
+           {
+               if (child.name != parts[0])
+               {
+                   continue;
+               }
+
+               switch (parts[1])
+               {
+                   default:
+                   case "on":
+                       child.gameObject.SetActive(true);
+                       break;
+                   case "off":
+                       child.gameObject.SetActive(false);
+                       break;
+               }
+               return;
+           }
+        }
+
+        public void SetTrigger(string nameAndKey)
+        {
+           var parts = nameAndKey.Split(':');
+
+           foreach (var child in _children)
+           {
+               if (child.name != parts[0])
+               {
+                   continue;
+               }
+
+               if (!child.gameObject.TryGetComponent<Animator>(out var animator))
+               {
+                   continue;
+               }
+               
+               animator.SetTrigger(parts[1]);
+               return;
+           }
+        }
+    }
+}
