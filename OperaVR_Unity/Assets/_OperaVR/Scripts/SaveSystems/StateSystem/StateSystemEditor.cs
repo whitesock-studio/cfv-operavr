5ad6ef6d9a5218cd7dd7d9b91c7ac1bd77b8891f
Gianni <>
2026-02-26 15:36:42 +0100

    fix: trackers ids

--- OperaVR_Unity/Assets/_OperaVR/Scripts/SaveSystems/StateSystem/StateSystemEditor.cs
+++ OperaVR_Unity/Assets/_OperaVR/Scripts/SaveSystems/StateSystem/StateSystemEditor.cs
@@ -1,4 +1,5 @@
 #if UNITY_EDITOR
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -15,14 +16,22 @@ namespace OperaVR
                 return;
             }
             var stateTrackers = FindObjectsOfType<AStateTracker>(true);
+            var usedIds = new List<string>();
             foreach (var stateTracker in stateTrackers)
             {
                 if (!string.IsNullOrEmpty(stateTracker.Id))
                 {
+                    if (usedIds.Contains(stateTracker.Id))
+                    {
+                        stateTracker.Id = GUID.Generate().ToString();
+                        EditorUtility.SetDirty(stateTracker);
+                    }
+                    usedIds.Add(stateTracker.Id);
                     continue;
                 }
 
                 stateTracker.Id = GUID.Generate().ToString();
+                EditorUtility.SetDirty(stateTracker);
             }
         }
     }
