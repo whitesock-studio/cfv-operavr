990fa896cbb16fd3c1adc2c811acc010f6850070
Gianni <>
2026-03-10 10:49:24 +0100

    chore: add path starter

---
+++ OperaVR_Unity/Assets/_OperaVR/Scripts/NPC/CharacterPathStarter.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+namespace OperaVR
+{
+    public class CharacterPathStarter : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _character;
+
+        [SerializeField]
+        private Path _path;
+
+        [SerializeField]
+        private float _movementSpeed = 3f;
+        
+        private int _currentPathIndex = -1;
+        
+        private const float STOP_DISTANCE = 0.1f;
+        
+        private void Update()
+        {
+            if (_currentPathIndex == -1)
+            {
+                return;
+            }
+            var destination = _path.Points[_currentPathIndex].position;
+            var movement = destination - _character.position;
+            var distanceToDestination = movement.magnitude;
+
+            if (distanceToDestination < STOP_DISTANCE)
+            {
+                Progress();
+                return;
+            }
+            
+            var direction = movement.normalized;
+            var directionNoZ = direction;
+            directionNoZ.y = 0;
+            directionNoZ.Normalize();
+            var frameSpeed = _movementSpeed * Time.deltaTime;
+            
+            if (distanceToDestination < frameSpeed)
+            {
+                _character.position = destination;
+                _character.rotation = Quaternion.LookRotation(directionNoZ);
+                return;
+            }
+
+            _character.position += direction * frameSpeed;
+            _character.rotation = Quaternion.Lerp(transform.rotation, 
+                Quaternion.LookRotation(directionNoZ), Time.deltaTime * 10f);
+        }
+
+        private void Progress()
+        {
+            _currentPathIndex++;
+            if (_currentPathIndex >= _path.Points.Length)
+            {
+                _currentPathIndex = _path.Loops ? 0 : -1;
+            }
+        }
+
+        public void StartPath()
+        {
+            _currentPathIndex = 0;
+        }
+    }
+}
